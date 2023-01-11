8c770b7cfd6f909e291932db9d37620bd3755c18
Julia Gondek <julia.gondek@hotmail.com>
2023-01-11 14:48:40 +0100

    GenerateRandomColor

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Node.js Random Color Generator
+
+> Generates randomly colored blocks with hex code
+
+## Usage
+
+Generate a random color
+
+´´´
+$ node index.js
+´´´
+
+Generate a specific color
+
+´´´
+$ node index.js blue
+´´´
+
+Generate color and its luminosity
+
+´´´
+§ node index.js blue dark
+´´´
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const hue = process.argv[2];
+const luminosity = process.argv[3];
+
+// Generate random color, luminosity and/or hue
+
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
+
+// Colored box with hex value
+
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('#####                     #####'));
+console.log(chalk.hex(color).bold(`#####       ${color}       #####`));
+console.log(chalk.hex(color).bold('#####                     #####'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
